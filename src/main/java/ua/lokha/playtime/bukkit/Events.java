8f4c51ff5baf76c57688d0ddd1b0a85893e53bc5
lokha <aleksey.kovalchuk98@gmail.com>
2020-01-09 19:50:22 +0200

    Фикс утечки

--- src/main/java/ua/lokha/playtime/bukkit/Events.java
+++ src/main/java/ua/lokha/playtime/bukkit/Events.java
@@ -1,13 +1,20 @@
 package ua.lokha.playtime.bukkit;
 
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 public class Events implements Listener {
 
     @EventHandler
     public void on(PlayerMoveEvent event) {
         Metadata.get(event.getPlayer()).setLastMove(System.currentTimeMillis());
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void on(PlayerQuitEvent event) {
+        Metadata.getPlayers().remove(event.getPlayer());
     }
 }
